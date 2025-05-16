f1f9e02dc49b3d39a1a1888015fbb567ce352f4c
Giwby Albatross <giwby.albatross@gmail.com>
2025-05-16 13:32:57 +1000

    Update deobfuscate_object_names.py

--- runtime/deobfuscate_object_names.py
+++ runtime/deobfuscate_object_names.py
@@ -1,18 +1,27 @@
-""" deobfuscate the contents of jars/objects which is mostly the music and sound effects
+#!/usr/bin/env python3
+""" deobfuscate the contents of jars/objects which is mostly the music and sound effects (program by Giwby Albatross)
 don't release the resulting files as it is a copyright issue according to https://github.com/WangTingZheng/mcp940/issues/2#issuecomment-2883062912 """
 
 import json
+import time
 import pathlib
 import shutil
 
 MC_VERSION = '1.12'
 
+start_time = time.time()
 assets_folder = pathlib.Path("jars", "assets")
 objects_folder= pathlib.Path(assets_folder, "objects")
 indexes_folder= pathlib.Path(assets_folder, "indexes")
 result_folder = pathlib.Path("objects")
+logfile = open(pathlib.Path("logs", "deobfuscate_assets.log"), 'w')
+
+def log(*args, **kwargs):
+    kwargs['file'] = logfile
+    print(f"[{time.time()-start_time}]", *args, **kwargs)
 
 def main(argv) -> int:
+    log("Reading index file:", str(indexes_folder / MC_VERSION+'.json'))
     mapping_file = open(indexes_folder / MC_VERSION+'.json')
     mappings = json.load(mapping_file)['objects']
     for file in mappings:
@@ -21,9 +30,10 @@ def main(argv) -> int:
         fileloc = hashdir / file_id
         endpath = result_folder / file
         end_dir = endpath.parents[0]
-
+        log("Putting file", file, "in", end_dir, "from hash:", file_id)
         end_dir.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(fileloc, endpath)
+    log.close()
 
 if __name__ == '__main__':
     import sys
