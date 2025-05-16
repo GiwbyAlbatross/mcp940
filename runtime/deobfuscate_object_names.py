309c2cbdd20e7d41e907bb1ed736daebbe908e32
Giwby Albatross <giwby.albatross@gmail.com>
2025-05-16 11:09:00 +1000

    You can now deobfuscate the names of these objects!

--- runtime/deobfuscate_object_names.py
+++ runtime/deobfuscate_object_names.py
@@ -2,17 +2,29 @@
 don't release the resulting files as it is a copyright issue according to https://github.com/WangTingZheng/mcp940/issues/2#issuecomment-2883062912 """
 
 import json
-from os import path
+import pathlib
+import shutil
 
 MC_VERSION = '1.12'
 
-assets_folder = path.join("jars", "assets")
-objects_folder= path.join(assets_folder, "objects")
-indexes_folder= path.join(assets_folder, "indexes")
+assets_folder = pathlib.Path("jars", "assets")
+objects_folder= pathlib.Path(assets_folder, "objects")
+indexes_folder= pathlib.Path(assets_folder, "indexes")
+result_folder = pathlib.Path("objects")
 
 def main(argv) -> int:
-    mapping_file = open(path.join(indexes_folder, MC_VERSION+'.json'))
+    mapping_file = open(indexes_folder / MC_VERSION+'.json')
     mappings = json.load(mapping_file)['objects']
+    for file in mappings:
+        file_id = mappings[file]['hash']
+        hashdir = objects_folder / file_id[:2]
+        fileloc = hashdir / file_id
+        endpath = result_folder / file
+        end_dir = endpath.parents[0]
+
+        end_dir.mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(fileloc, endpath)
+        
 
 if __name__ == '__main__':
     import sys
